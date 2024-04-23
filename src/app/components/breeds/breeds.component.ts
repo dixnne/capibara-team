3ce471f6ef8970fc066bb4692b718930cd3fe1c8
Diana Narvaez <dianapa.narvaez@gmail.com>
2024-04-23 14:15:26 -0600

    Add navbar (first version)

---
+++ src/app/components/breeds/breeds.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breeds',
+  standalone: true,
+  imports: [],
+  templateUrl: './breeds.component.html',
+  styleUrl: './breeds.component.css'
+})
+export class BreedsComponent {
+
+}
