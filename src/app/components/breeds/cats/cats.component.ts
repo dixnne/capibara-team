cb5d25cc9ea168ee0eec2a5a74dfb15764429978
Diana Narvaez <dianapa.narvaez@gmail.com>
2024-04-24 18:58:12 -0600

    Add API implementations for breeds info

--- src/app/components/breeds/cats/cats.component.ts
+++ src/app/components/breeds/cats/cats.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { CatBreedsService } from '../../../shared/cat-breeds.service';
+import { CommonModule } from '@angular/common';
+import { BreedComponent } from '../breed/breed.component';
 
 @Component({
   selector: 'app-cats',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, BreedComponent],
   templateUrl: './cats.component.html',
   styleUrl: './cats.component.css'
 })
 export class CatsComponent {
 
+  breeds: any = [];
+  activeImage: any = {
+    url: "",
+    height: 0,
+    width: 0
+  }
+
+  constructor(public catBreedsService: CatBreedsService) {
+    this.retrieveData();
+  }
+
+  retrieveData(): void {
+    console.log("Connecting to Cat Breeds API");
+    this.catBreedsService.returnBreedsList().subscribe({
+      next: this.successRequest.bind(this),
+      error: (err) => {console.log(err)}
+    });
+  }
+  
+  successRequest(data: any): void {
+    console.log(data);
+    this.breeds = data;
+  }
+
+  getBreedImage(breedID: string): any {
+    console.log("Getting Breed Image (id = " + breedID + ")");
+    this.catBreedsService.returnABreed(breedID).subscribe({
+      next: this.successRequestImage.bind(this),
+      error: (err) => {console.log(err)}
+    });
+  }
+
+  successRequestImage(data: any): void {
+    console.log(data);
+    this.activeImage.url = data[0].url;
+    this.activeImage.width = data[0].width;
+    this.activeImage.height = data[0].height;
+  }
 }
--- src/app/shared/cat-breeds.service.ts
+++ src/app/shared/cat-breeds.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,14 @@ import { Injectable } from '@angular/core';
 })
 export class CatBreedsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  returnBreedsList() {
+    return this.http.get("https://api.thecatapi.com/v1/breeds");
+  }
+
+  returnABreed(breedId: string) {
+    return this.http.get("https://api.thecatapi.com/v1/images/search?breed_ids=" + breedId);
+  }
 }
+
