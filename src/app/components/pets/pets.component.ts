a68850fe4e8d7f48346fd3ec934ad646e0522253
Diana Narvaez <dianapa.narvaez@gmail.com>
2024-05-05 18:35:28 -0600

    Pets array update and redesign in pet cards

--- src/app/components/pets/pets.component.ts
+++ src/app/components/pets/pets.component.ts
@@ -3,11 +3,12 @@ import { Pet } from '../../interfaces/pet';
 import { PetsService } from '../../shared/pets.service';
 import { RouterLink } from '@angular/router';
 import { PaginatorModule } from 'primeng/paginator';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pets',
   standalone: true,
-  imports: [RouterLink, PaginatorModule],
+  imports: [RouterLink, PaginatorModule, CommonModule],
   templateUrl: './pets.component.html',
   styleUrl: './pets.component.css',
 })
