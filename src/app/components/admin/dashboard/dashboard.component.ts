c0c918fb3497f0d86db00e3a8c19dc8a23824b0c
Diana Dávila <memv.cm@gmail.com>
2024-06-24 23:49:17 -0600

    Funcion para graficas

--- src/app/components/admin/dashboard/dashboard.component.ts
+++ src/app/components/admin/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { AdminGraphComponent } from '../admin-graph/admin-graph.component';
 import { Graph1Component } from '../graph1/graph1.component';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +11,79 @@ import { Graph1Component } from '../graph1/graph1.component';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  
+  ngOnInit(): void {
+    this.RenderChart();
+    this.Render2Chart();
+  }
 
+  RenderChart(){    
+    const myChart = new Chart("myChart", {
+
+    type: 'line',
+    data: {
+      labels:['Monday', 'Tuesday', 'Wednesday', 
+        'Thursday', 'Friday', 'Saturday', 'Sunday'],
+      datasets: [{
+        label: 'Registers',
+        data: [65, 59, 80, 81, 56, 55, 40, 150],
+        // fill: false,
+        borderColor: 'rgb(255, 199, 0)',
+        // tension: 0.1
+      }]
+    },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+
+  Render2Chart(){    
+    const myChart = new Chart("graph2", {
+
+    type: 'bar',
+    data: {
+      labels:['Monday', 'Tuesday', 'Wednesday', 
+        'Thursday', 'Friday', 'Saturday', 'Sunday'],
+      datasets: [{
+        label: 'Visits',
+        data: [65, 59, 80, 81, 56, 55, 40, 200],
+        // fill: false,
+
+        backgroundColor: [
+          'rgba(151, 49, 49)',
+          'rgba(255, 125, 41)',
+          'rgb(255, 175, 69)',
+          'rgb(224, 167, 94)',
+          'rgb(111, 78, 55)',
+          'rgb(221, 118, 28)',
+          'rgb(239, 156, 102)'
+        ],
+        borderColor: [
+          'rgb(151, 49, 49)',
+          'rgb(255, 159, 64)',
+          'rgb(255, 175, 69)',
+          'rgb(224, 167, 94)',
+          'rgb(111, 78, 55)',
+          'rgb(221, 118, 28)',
+          'rgb(239, 156, 102)'
+        ],
+        borderWidth: 1,
+        // tension: 0.1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+    });
+  }
 }
