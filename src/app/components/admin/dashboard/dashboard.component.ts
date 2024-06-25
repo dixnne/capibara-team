9dbc70d5a84f4196dce722cf89cd1b6f8d48f13c
Diana Dávila <memv.cm@gmail.com>
2024-06-25 00:10:13 -0600

    Cambios en Class DashBoard

--- src/app/components/admin/dashboard/dashboard.component.ts
+++ src/app/components/admin/dashboard/dashboard.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { SidebarComponent } from '../sidebar/sidebar.component';
-import { AdminGraphComponent } from '../admin-graph/admin-graph.component';
-import { Graph1Component } from '../graph1/graph1.component';
 import { Chart } from 'chart.js/auto';
 import { CalendarComponent } from '../../dates/calendar/calendar.component';
 import { CalendarModule } from 'primeng/calendar';
@@ -12,11 +10,24 @@ import { GraphService } from '../../../shared/graph/graph.service';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [SidebarComponent, AdminGraphComponent, Graph1Component,CalendarComponent,CalendarModule,FormsModule],
+  imports: [SidebarComponent, CalendarComponent,CalendarModule,FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
+
+  selectedDate?:Date;
+  yearRange?: string;
+  constructor(private grphService:GraphService){
+    const currentYear = new Date().getFullYear();
+    this.yearRange = `${currentYear - 10}:${currentYear + 10}`; // Ajusta el rango de años según sea necesario
+  }
+  cambiarFecha(date:Date){
+    let pipe=new DatePipe('en-US');
+    this.grphService.getChartsData(pipe.transform(date,'yyyy-MM-dd')!).subscribe((data:any)=>{
+      console.log(data);
+    })
+  }
   
   ngOnInit(): void {
     this.RenderChart();
@@ -34,7 +45,7 @@ export class DashboardComponent implements OnInit {
         label: 'Registers',
         data: [65, 59, 80, 81, 56, 55, 40, 150],
         // fill: false,
-        borderColor: 'rgb(255, 199, 0)',
+        borderColor: 'rgb(255, 199, 0)'
         // tension: 0.1
       }]
     },
@@ -78,7 +89,7 @@ export class DashboardComponent implements OnInit {
           'rgb(221, 118, 28)',
           'rgb(239, 156, 102)'
         ],
-        borderWidth: 1,
+        borderWidth: 1
         // tension: 0.1
       }]
     },
@@ -90,19 +101,5 @@ export class DashboardComponent implements OnInit {
       }
     }
     });
-  }
-}
-export class DashboardComponent {
-  selectedDate?:Date;
-  yearRange?: string;
-  constructor(private grphService:GraphService){
-    const currentYear = new Date().getFullYear();
-    this.yearRange = `${currentYear - 10}:${currentYear + 10}`; // Ajusta el rango de años según sea necesario
-  }
-  cambiarFecha(date:Date){
-    let pipe=new DatePipe('en-US');
-    this.grphService.getChartsData(pipe.transform(date,'yyyy-MM-dd')!).subscribe((data:any)=>{
-      console.log(data);
-    })
   }
 }
