cdc391e93102f9318057807ec1101f44e3f535ac
TaniaMontserrat <montserrat.ma.es.42@gmail.com>
2024-06-24 19:59:58 -0600

    user service

--- src/app/components/admin/add-user/add-user.component.ts
+++ src/app/components/admin/add-user/add-user.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { FormsModule } from '@angular/forms';
+import { UserService } from '../../../shared/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-user',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, FormsModule],
   templateUrl: './add-user.component.html',
-  styleUrl: './add-user.component.css'
+  styleUrl: './add-user.component.css',
 })
 export class AddUserComponent {
-
+  user: any;
+  tag: string = '';
+  currentFile?: File;
+  constructor(private users: UserService, private router: Router) {}
 }
---
+++ src/app/shared/user.service.ts
@@ -0,0 +1,37 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  url: string = 'https://capibara.losnarvaez.com/';
+
+  constructor(public http: HttpClient) {}
+
+  addUser(newUser: User, file: File) {
+    const formData: FormData = new FormData();
+    formData.append('file', file);
+    formData.append('pet', JSON.stringify(newUser.data));
+    let headers = new HttpHeaders();
+    headers = headers.append('enctype', 'multipart/form-data');
+    console.log(formData);
+    return this.http.post<any>(this.url + 'users', formData, {
+      headers: headers,
+    });
+  }
+
+  deleteUser(id: string) {
+    return this.http.delete<any>(this.url + 'users/' + id);
+  }
+  getUsers() {
+    return this.http.get<User[]>(this.url + 'users');
+  }
+  getUser(id: string) {
+    return this.http.get<User>(this.url + 'users/' + id);
+  }
+  getUserByName(name: string) {
+    return this.http.get<User>(this.url + 'users?name=' + name);
+  }
+}
