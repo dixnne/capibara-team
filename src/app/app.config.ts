42c0b2412829504f617f0fd7fa30868c146b111d
Diana Narvaez <dianapa.narvaez@gmail.com>
2024-06-25 14:55:01 -0600

    Add PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { LoadingInterceptor } from './loading.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
-    provideClientHydration(), 
-    provideHttpClient(
-      withInterceptors([LoadingInterceptor])
-    )
-  ]
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withInterceptors([LoadingInterceptor])),
+    provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })
+]
 };
